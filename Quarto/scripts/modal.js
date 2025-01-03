371b4537890e19b7933cb387ead98dedd85c867a
misbah <misbah.electrochem@gmail.com>
2025-01-03 12:51:27 +0500

    fdrsgt

--- Quarto/scripts/modal.js
+++ Quarto/scripts/modal.js
@@ -1,12 +1,29 @@
-// scripts.js
-document.addEventListener("DOMContentLoaded", function() {
-    const toggleLinks = document.querySelectorAll('.timeline a');
+<script>
+// Get the modal
+function showbox(buttonid, modalid){
+  var modal = document.getElementById(modalid);
 
-    toggleLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            const content = event.target.closest('.timeline').querySelector('.content');
-            content.classList.toggle('show');
-        });
-    });
-});
+  // Get the button that opens the modal
+  var btn = document.getElementById(buttonid);
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks the button, open the modal 
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+</script>
